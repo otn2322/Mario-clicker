207fc04316b1f710ddccbde57b6334137b155f1b
otn2322 <otn.andreassen@hotmail.com>
2021-11-10 15:20:41 +0100

    Change 7
    Jeg har addet 2 noe oppgraderings hjelpemidler, skal fortsette med det.

--- mario.js
+++ mario.js
@@ -1,7 +1,7 @@
 var coins = 0;
 
 var peach_value_each = 5;
-var peachamout = 0;
+var peachamount = 0;
 var peachincrease = 0;
 var peach_price = 10;
 
@@ -19,11 +19,30 @@ var ghost_amount = 0;
 var ghost_increase = 0;
 var ghost_price = 10000;
 
+var toad_value_each = 5000;
+var toad_amount = 0;
+var toad_increase = 0;
+var toad_price = 100000;
+
+var mario_value_each = 2000;
+var mario_amount = 0;
+var mario_increase = 0;
+var mario_price = 500000;
+
+var bowserjr_value_each = 4000;
+var bowserjr_amount = 0;
+var bowserjr_increase = 0;
+var bowserjr_price = 2500000;
+
+
 
 document.getElementById("peach_price").innerHTML = peach_price + "$";
 document.getElementById("shyguy_price").innerHTML = shyguy_price + "$";
 document.getElementById("oneup_price").innerHTML = oneup_price + "$";
 document.getElementById("ghost_price").innerHTML = ghost_price + "$";
+document.getElementById("toad_price").innerHTML = toad_price + "$";
+document.getElementById("mario_price").innerHTML = mario_price + "$";
+document.getElementById("bowserjr_price").innerHTML = bowserjr_price + "$";
 
 
 
@@ -36,7 +55,7 @@ var intervalId = window.setInterval(function(){
 
 var intervalId2 = window.setInterval(function(){
     // leger verdi ifra peach in til coins
-    peachincrease = peachamout * peach_value_each;
+    peachincrease = peachamount * peach_value_each;
     coins += peachincrease;
 }, 1000);
 
@@ -55,11 +74,33 @@ var intervalId4 = window.setInterval(function(){
 }, 6000);
 
 var intervalId5 = window.setInterval(function(){  
-    // 
+    // leger verdi ifra ghost in til coins
     ghost_increase = ghost_amount * ghost_value_each;
     coins += ghost_increase;
    }, 6000);
 
+var intervalId6 = window.setInterval(function(){
+    // leger verdi ifra toad in til coins
+    toad_increase = toad_amount * toad_value_each;
+    coins += toad_increase;
+}, 3000);
+
+var intervalId7 = window.setInterval(function(){
+    // leger verdi ifra mario in til coins
+    mario_increase = mario_amount * mario_value_each;
+    coins += mario_increase;
+}, 2000);
+
+var intervalId8 = window.setInterval(function(){
+    // leger verdi ifra bowserjr in til coins
+    bowserjr_increase = bowserjr_amount * bowserjr_value_each;
+    coins += bowserjr_increase;
+}, 2000);
+
+
+function amount_bought() {
+    document.getElementById("peachamout").innerHTML = peachamount;
+}
 
 
 function coinbutton() {
@@ -70,7 +111,7 @@ function coinbutton() {
 
 function peach() {
 if (coins >= peach_price){
-    peachamout += 1;
+    peachamount += 1;
     coins -= peach_price;
     peach_price *= 3;
     document.getElementById("peach_price").innerHTML = peach_price + "$";
@@ -111,5 +152,32 @@ function ghost() {
         ghost_price *= 2;
         document.getElementById("ghost_price").innerHTML = ghost_price + "$";
 
+    }
+}
+
+function toad() {
+    if (coins >= toad_price) {
+        toad_amount += 1;
+        coins -= toad_price;
+        toad_price *= 2;
+        document.getElementById("toad_price").innerHTML = toad_price + "$"
+    }
+}
+
+function mario() {
+    if (coins >= mario_price) {
+        mario_amount += 1;
+        coins -= mario_price;
+        mario_price *= 2;
+        document.getElementById("mario_price").innerHTML = mario_price + "$"
+    }
+}
+
+function bowserjr(){
+    if (coins >= bowserjr_price) {
+        bowserjr_amount += 1;
+        coins -= bowserjr_price;
+        bowserjr_price *= 2;
+        document.getElementById("bowserjr_price").innerHTML = bowserjr_price + "$"
     }
 }
